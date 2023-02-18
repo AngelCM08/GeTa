ac303168e759c7d60d4b3b5610233ec552f4c2ba
AngelCM08 <92088141+AngelCM08@users.noreply.github.com>
2023-02-18 21:46:37 +0100

    commit

--- app/src/main/java/com/example/geta/FrontPageFragment.java
+++ app/src/main/java/com/example/geta/FrontPageFragment.java
@@ -1,17 +1,22 @@
 package com.example.geta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class FrontPageFragment extends AppCompatActivity {
+    private static final long SPLASH_SCREEN_DELAY = 3000;
     private ImageView logo;
-    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +35,17 @@ public class FrontPageFragment extends AppCompatActivity {
             public void onAnimationEnd(Animation animation) {
                 logo.clearAnimation(); // Limpia la animación para que la imagen no vuelva a su posición anterior
                 logo.setTranslationY(30); // Cambia la posición de la imagen 200 píxeles hacia abajo
-                navController.navigate(R.id.loginFragment);
             }
             @Override
             public void onAnimationRepeat(Animation animation) {}
         });
 
         logo.startAnimation(animacion);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_SCREEN_DELAY);
     }
 }
